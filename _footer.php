4347f386b1206156bf6149f03e5c629f7d24da53
Akshata Kodak <85898174+akshatak1105@users.noreply.github.com>
2023-05-03 00:05:50 +0530

    Add files via upload

---
+++ _footer.php
@@ -0,0 +1,2 @@
+<div class="container-fluid bg-dark text-light" > 
+  <p class="text-center mb-0" >Copyright Quora 2023 || All rights reserved </p> </div>
---
+++ about.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Welcome to VIT Quora</title>
+</head>
+
+<body>
+    <?php include 'partials/_dbconnect.php';?>
+    <?php include 'partials/_header.php';?>
+
+    <section class="about1" style="width: 100%;
+    /* background-image: linear-gradient(rgba(4,9,30,0.7),rgba(4,9,30,0.7)); */
+    background-size: cover;
+
+    color:black;
+    position: absolute;
+    margin-top:20px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    text-align:center;">
+        <h3 style="font-size: 30px;
+        margin-top:20px;
+        font:bold; ">ABOUT</h3><br>
+        <pre style="margin: 10px 0 40px ;
+                    font-size: 15px;
+                    color:black;
+                    text-align:center;
+                    ">
+        College: Vishwakarma Institute of Technology, Pune, a highly commendable private institute,
+            occupies a place of pride amongst the premier technical institutes of the western region of 
+            India. Established in the year 1983, financed and run by the Bansilal Ramnath Agrawal Charitable Trust, Pune.
+            It is affiliated to the University of Pune . Within three decades, 
+            the institute marched towards the pinnacle of glory through its remarkable achievements and laurels
+            in the field of engineering education of high calibre. According to Indian history,
+            'Vishwakarma' is a divine architect-engineer.
+            It is believed that He fashioned this world with His rare engineering skills.
+            The Trust adopted this name with a vision to develop engineers of high calibre,
+            who could take up challenges of any type of engineering job and become successful in the chosen career. 
+            The average salary package offered in placements of VIT,Pune is 8 LPA,
+            Around 80% of the students get internships from the college in good companies.
+            Few Companies are CREDIT SUISSE, NVIDIA, TOMTOM, Deutsche Bank, Siemens.
+            
+            Website: VIT Quora will help you out to join a community of students in VIT where one can post a question 
+            and answer on questions posted by others.Students can discuss on 
+            various topics and get answers to it.One can also search whatever comes to his mind.
+
+        </pre>
+        <?php include 'partials/_footer.php';?>
+
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
